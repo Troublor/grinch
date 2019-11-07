0cc50ba1f7040113d82cbfe13b707b56712e3f65
troublor <troublor@live.com>
2019-11-07 17:33:26 +0800

    implement navigable small world graphs to facilitate neighbor search

--- linkage/single.py
+++ linkage/single.py
@@ -1,10 +1,13 @@
 import sys
 
 from model.cluster import Cluster
+from model.data_point import TrivialDataPoint
 
 
 def single_linkage(c1: Cluster, c2: Cluster) -> float:
     max_value = -sys.float_info.max
+    if isinstance(c1, TrivialDataPoint):
+        print()
     for p1 in c1.data_points:
         for p2 in c2.data_points:
             s = p1.pairwise_similarity(p2)
---
+++ nsw/graph.py
@@ -0,0 +1,68 @@
+import random
+import sys
+from typing import List, Callable, Union
+
+import dendrogram
+from model.cluster import Cluster
+from model.data_point import DataPoint
+from .node import Node
+
+
+class Graph(object):
+    def __init__(self, k: int, f: Callable[[Cluster, Cluster], float]):
+        self._nodes: List[Node] = []
+        self._k = k
+        self.f = f
+
+    def add_data_point(self, data_point: DataPoint) -> Node:
+        tmp = []
+        node = Node(data_point)
+        for n in self._nodes:
+            tmp.append((n, self.f(n, node)))
+        tmp.sort(key=lambda elem: elem[1], reverse=True)
+        for n in tmp[:self._k]:
+            node.add_neighbor(n[0], n[1])
+        self._nodes.append(node)
+        return node
+
+    def nearest_neighbor(self, node: Node) -> Union[Node, None]:
+        return self.constr_nearest_neighbor(node, exclude=[node])
+
+    def constr_nearest_neighbor(self, node: dendrogram.Node, exclude: List[dendrogram.Node] = None) \
+            -> Union[Node, None]:
+        if exclude is None:
+            exclude = [node]
+        elif node not in exclude:
+            exclude.append(node)
+        tmp = list(self._nodes)
+        for ex_node in exclude:
+            if ex_node in tmp:
+                tmp.remove(ex_node)
+        if len(tmp) == 0:
+            return None
+        v = tmp[random.randint(0, len(tmp) - 1)]
+        neighbors = v.similar_neighbors(1, exclude)
+        if len(neighbors) == 0:
+            return v
+        while self.get_similarity(v, node) < self.get_similarity(v, neighbors[0]):
+            max_value = -sys.float_info.max
+            nearest = None
+            for neighbor in v.similar_neighbors(len(v.neighbors), exclude):
+                sim = self.get_similarity(node, neighbor)
+                if sim > max_value:
+                    max_value = sim
+                    nearest = neighbor
+            v = nearest
+            neighbors = v.similar_neighbors(1, exclude)
+            if len(neighbors) == 0:
+                break
+        return v
+
+    def get_similarity(self, n1: Union[dendrogram.Node, Node], n2: Union[dendrogram.Node, Node]) -> float:
+        if n1 in self._nodes and n2 in self._nodes:
+            t = n1.similarity(n2)
+        else:
+            t = self.f(n1, n2)
+        if t is None:
+            print()
+        return t
