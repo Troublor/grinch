4ef1f299bceddbcf81ed4d1b82f2c1412ad9036f
troublor <troublor@live.com>
2019-11-08 17:33:27 +0800

    using numpy to optimize

--- synthetic.py
+++ synthetic.py
@@ -38,38 +38,31 @@ def data_wrapper(dataset, n_cluster: int) -> Tuple[List[DataPoint], List[GroundT
 
 
 def vector_cosine(v1: List[int], v2: List[int]):
-    assert len(v1) == len(v2)
-    ab = 0
-    a2 = 0
-    b2 = 0
-    for i in range(len(v1)):
-        ab += v1[i] * v2[i]
-        a2 += v1[i] ** 2
-        b2 += v2[i] ** 2
-    d = (math.sqrt(a2) * math.sqrt(b2))
-    if d == 0:
-        return -sys.float_info.max + 1
-    return ab / d
+    return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
 
 
 def cosine_similarity(c1: Cluster, c2: Cluster) -> float:
-    total1 = np.array(c1.data_points[0].vector)
-    for dp in c1.data_points[1:]:
-        assert isinstance(dp, BinaryDataPoint)
-        total1 += np.array(dp.vector)
-    total2 = np.array(c2.data_points[0].vector)
-    for dp in c2.data_points[1:]:
-        assert isinstance(dp, BinaryDataPoint)
-        total2 += np.array(dp.vector)
-    return vector_cosine(total1, total2)
+    v1 = list(map(lambda elem: elem.vector, c1.data_points))
+    v2 = list(map(lambda elem: elem.vector, c2.data_points))
+    return vector_cosine(np.sum(v1, axis=0), np.sum(v2, axis=0))
+    # total1 = np.array(c1.data_points[0].vector)
+    # for dp in c1.data_points[1:]:
+    #     assert isinstance(dp, BinaryDataPoint)
+    #     total1 += np.array(dp.vector)
+    # total2 = np.array(c2.data_points[0].vector)
+    # for dp in c2.data_points[1:]:
+    #     assert isinstance(dp, BinaryDataPoint)
+    #     total2 += np.array(dp.vector)
+    # return vector_cosine(total1, total2)
     # return vector_cosine(c1.cache.tolist(), c2.cache.tolist())
 
 
 gen = DataGeneration()
-n_cluster = 4
-n_point_each_cluster = 50
+n_cluster = 25
+n_point_each_cluster = 5
 n_dim_datapoint = 2500
-output = gen.gen_random_dataset(n_cluster=n_cluster, n_point_each_cluster=n_point_each_cluster, n_dim_datapoint=n_dim_datapoint)
+output = gen.gen_random_dataset(n_cluster=n_cluster, n_point_each_cluster=n_point_each_cluster,
+                                n_dim_datapoint=n_dim_datapoint)
 data_stream, ground_truth = data_wrapper(output, n_cluster)
 
 grinch = Grinch(cosine_similarity, debug=False, navigable_small_world_graphs=False, k_nsw=30)
