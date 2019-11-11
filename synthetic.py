55ad31d0ebd34d0ebcdfb537e6cb400fc9893b99
troublor <troublor@live.com>
2019-11-11 17:26:21 +0800

    ready for synthetic experiment

--- synthetic.py
+++ synthetic.py
@@ -53,33 +53,68 @@ def cosine_similarity(c1: Cluster, c2: Cluster) -> float:
     return vector_cosine(np.sum(v1, axis=0), np.sum(v2, axis=0))
 
 
-gen = DataGeneration(shuffle=random_shuffle)
-n_cluster = 20
-n_point_each_cluster = 25
-n_dim_datapoint = 2500
-output = gen.gen_random_dataset(n_cluster=n_cluster, n_point_each_cluster=n_point_each_cluster,
-                                n_dim_datapoint=n_dim_datapoint)
-data_stream, ground_truth = data_wrapper(output, n_cluster)
+def param_print(*params):
+    for param in params:
+        print(param, '=', repr(eval(param)))
 
-clustering = Grinch(cosine_similarity, debug=False, single_nn_search=False, k_nn=25, single_elimination=False,
-                capping=False, capping_height=100)
-# clustering = OnlineHAC(cosine_similarity)
-# clustering = RotationHAC(cosine_similarity)
-monitor = DpMonitor(n_data_points=len(data_stream), n_workers=8, ground_truth=ground_truth)
-count = 0
-start = time.time()
-for dp in data_stream:
-    print("insert data point", count)
-    clustering.insert(dp)
-    cp = copy.deepcopy(clustering.dendrogram)
-    monitor.feed(count, cp)
-    count += 1
-clustering.dendrogram.print()
-end = time.time()
-# print("rotation:", grinch.rotation_count)
-# print("graft:", grinch.graft_count)
-# print("restruct:", grinch.restruct_count)
-# print("similarity:", grinch.similarity_count)
-# print("reuse:", grinch.similarity_reused_count)
-print("time:", end - start)
-print("dp: ", dendrogram_purity(ground_truth, clustering.dendrogram))
+
+stdout = sys.stdout
+with open("experiment/synthetic/grinch.txt", "w") as file:
+    sys.stdout = file
+
+    total_time = 0
+    n = 5
+    for i in range(n):
+        gen = DataGeneration(shuffle=random_shuffle)
+        n_cluster = 20
+        n_point_each_cluster = 25
+        n_dim_datapoint = 10000
+        param_print("n_cluster", "n_point_each_cluster", "n_dim_datapoint")
+
+        single_nn_search = False
+        k_nn = 25
+        param_print("single_nn_search", "k_nn")
+
+        single_elimination = False,
+        param_print("single_elimination")
+
+        capping = False
+        capping_height = 100
+        param_print("capping", "capping_height")
+
+        navigable_small_world_graphs = False
+        k_nsw = 50
+        param_print("navigable_small_world_graphs", "k_nsw")
+
+        output = gen.gen_random_dataset(n_cluster=n_cluster, n_point_each_cluster=n_point_each_cluster,
+                                        n_dim_datapoint=n_dim_datapoint)
+        data_stream, ground_truth = data_wrapper(output, n_cluster)
+
+        clustering = Grinch(cosine_similarity, debug=False, single_nn_search=single_nn_search, k_nn=k_nn,
+                            single_elimination=single_elimination,
+                            capping=capping, capping_height=capping_height,
+                            navigable_small_world_graphs=navigable_small_world_graphs, k_nsw=k_nsw)
+        # clustering = OnlineHAC(cosine_similarity)
+        # clustering = RotationHAC(cosine_similarity)
+        # monitor = DpMonitor(n_data_points=len(data_stream), n_workers=8, ground_truth=ground_truth)
+        count = 0
+        start = time.time()
+        for dp in data_stream:
+            # print("insert data point", count)
+            clustering.insert(dp)
+            # cp = copy.deepcopy(clustering.dendrogram)
+            # monitor.feed(count, cp)
+            count += 1
+        # clustering.dendrogram.print()
+        end = time.time()
+        # print("rotation:", grinch.rotation_count)
+        # print("graft:", grinch.graft_count)
+        # print("restruct:", grinch.restruct_count)
+        # print("similarity:", grinch.similarity_count)
+        # print("reuse:", grinch.similarity_reused_count)
+        print("clustering time:", end - start)
+        total_time += end - start
+        print("dendrogram purity: ", dendrogram_purity(ground_truth, clustering.dendrogram))
+        print("=======================================================================================================")
+    print("average time:", total_time / n)
+sys.stdout = stdout
