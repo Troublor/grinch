2c27b43461644b63f61db980210a3e8c07b50d96
ArabelaTso <arabela_tso@outlook.com>
2019-11-11 22:26:58 +0800

    read ALOI dataset

--- gendataset/realworld-dataset.py
+++ gendataset/realworld-dataset.py
@@ -1,3 +1,4 @@
+import re
 import numpy as np
 import glob
 from scipy import ndimage
@@ -5,22 +6,28 @@ import imageio
 
 
 class DataProcessor(object):
-    def __init__(self, dirname):
+    def __init__(self, dirname='../aloi-500-balance'):
         self.dir = dirname
 
     def read_imgs(self):
         imageset = []
-        for image_path in glob.glob("{}/*.png".format(self.dir)):
-            # image = np.array(ndimage.imread(image_path, flatten=False))
+        index = []
+        extension = '*.png'
+        for image_path in glob.glob("{}/*/*.png".format(self.dir)):
+            cls_index = re.findall(r'(\d*)_', image_path.split('\\')[-1])
             image = imageio.imread(image_path)
-            # print(image.shape)
             image = np.reshape(image, (image.shape[0] * image.shape[1], 1))
-            # print(image.shape)
             imageset.append(image)
+            index.append(int(cls_index[0]))
+            # print('index = {}'.format(cls_index))
+            # print(image_path)
+
+        assert len(index) == len(imageset)
         print('Read {} images.'.format(len(imageset)))
         print('Dimension of image: {}.'.format(len(imageset[0])))
-        return imageset
+
+        return imageset, index
 
 
-# dprocessor = DataProcessor('../aloi-500')
-# dprocessor.read_imgs()
+# dprocessor = DataProcessor('../aloi-500-balance')
+# # dprocessor.read_imgs()
