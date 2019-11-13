64c54fa81b86e305777973447ec296fbded70834
troublor <troublor@live.com>
2019-11-13 11:39:48 +0800

    backup

---
+++ test.py
@@ -0,0 +1,36 @@
+import json
+from itertools import accumulate
+
+from matplotlib import pyplot as plt
+
+# grinch graft analysis
+with open("experiment/graft_significance/grinch.json") as file:
+    s = file.readlines()
+    s = "".join(s)
+    data = json.loads(s)
+    x = [i + 1 for i in range(len(data))]
+    y_instant = list(map(lambda item: item[1] - item[0], data))
+    y_accumu = list(accumulate(y_instant, lambda a, b: a + b))
+    print(data)
+    print(y_instant)
+    print(y_accumu)
+    plt.plot(x, y_instant, 'r--', marker="o", label='instantaneous')
+    plt.plot(x, y_accumu, 'b--', marker="o", label='accumulate')
+    plt.xlabel('Data Points')  # X轴标签
+    plt.ylabel("Dendrogram Purity Change")  # Y轴标签
+    plt.legend()
+    plt.show()
+
+    # joint analysis
+    with open("experiment/graft_significance/rotation.json") as file1:
+        s = file1.readlines()
+        s = "".join(s)
+        data1 = json.loads(s)
+        y_grinch = list(map(lambda item: item[1], data))
+        y_rotation = list(map(lambda item: item[1], data1))
+        plt.plot(x, y_grinch, 'r--', marker="o", label='grinch')
+        plt.plot(x, y_rotation, 'b--', marker="o", label='rotation')
+        plt.xlabel('Data Points')  # X轴标签
+        plt.ylabel("Dendrogram Purity")  # Y轴标签
+        plt.legend()
+        plt.show()
