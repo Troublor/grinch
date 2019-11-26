b7dc4a2c3aa594a08dc3546197e3dd5ad4c0e709
troublor <troublor@live.com>
2019-11-26 20:10:32 +0800

    add Grinch algorithm usage samples

---
+++ ALOI_dataset_sample.py
@@ -0,0 +1,69 @@
+from typing import Tuple, List
+
+import numpy as np
+
+from clustering.evaluation import dendrogram_purity
+from clustering.grinch import Grinch
+from gendataset.realworld_dataset import DataProcessor
+from gendataset.shuffle import random_shuffle
+from linkage.vector import cosine_similarity_for_binary_vector_data_point
+from model.cluster import GroundTruthCluster
+from model.data_point import BinaryVectorDataPoint, DataPoint
+from monitor.dendrogram_purity import DpMonitor
+
+
+def data_wrapper(dataset) -> Tuple[List[DataPoint], List[GroundTruthCluster]]:
+    n_cluster = 0
+    for index in dataset[0]:
+        if index >= n_cluster:
+            n_cluster = index + 1
+    count = [0 for i in range(n_cluster)]
+    cc = [[] for i in range(n_cluster)]
+    data_stream = []
+    for index, cluster in enumerate(dataset[0]):
+        count[cluster] += 1
+        dp = BinaryVectorDataPoint(dataset[1][index], str(cluster) + "-" + str(count[cluster]))
+        cc[cluster].append(dp)
+        data_stream.append(dp)
+    clusters = []
+    for c in cc:
+        clusters.append(GroundTruthCluster(c))
+    return data_stream, clusters
+
+
+if __name__ == "__main__":
+    # read dataset from ALOI
+    gen = DataProcessor(dirname="./aloi-500-balance")
+    output = gen.read_imgs(shuffle=random_shuffle)  # other shuffle functions are provided in gendataset/shuffle.py
+    data_stream, ground_truth = data_wrapper(output)
+
+    # settings of Grinch algorithm
+    single_nn_search = True
+    k_nn = 25
+
+    single_elimination = True,
+
+    capping = Tuple
+    capping_height = 100
+
+    navigable_small_world_graphs = False
+    k_nsw = 50
+
+    # the purity monitor to monitor the dendrogram purity during inserting data points
+    monitor = DpMonitor(n_data_points=len(data_stream), n_workers=8, ground_truth=ground_truth)
+    # grinch approach to cluster
+    # other linkage functions are provided in linkage/*.py
+    # customized linkage function is also allowed as long as it takes same inputs and return same outputs.
+    clustering = Grinch(cosine_similarity_for_binary_vector_data_point, debug=False,
+                        single_nn_search=single_nn_search, k_nn=k_nn,
+                        single_elimination=single_elimination,
+                        capping=capping, capping_height=capping_height,
+                        navigable_small_world_graphs=navigable_small_world_graphs, k_nsw=k_nsw, monitor=monitor)
+    # process data stream
+    for dp in data_stream:
+        clustering.insert(dp)
+    # wait for monitor to finish its tasks (because it's multiprocessing)
+    monitor.join()
+    monitor.show_purity_plot()
+    clustering.dendrogram.print()
+    print("dendrogram purity:", dendrogram_purity(ground_truth, clustering.dendrogram))
---
+++ synthetic_dataset_sample.py
@@ -0,0 +1,76 @@
+import copy
+import unittest
+from typing import Tuple, List
+
+import numpy as np
+
+from clustering.evaluation import dendrogram_purity
+from clustering.grinch import Grinch
+from gendataset.generate_dataset import DataGeneration
+from gendataset.shuffle import random_shuffle
+from linkage.vector import cosine_similarity_for_binary_vector_data_point
+from model.cluster import GroundTruthCluster
+from model.data_point import BinaryVectorDataPoint, DataPoint
+
+
+def is_zero_vector(vector: List[int]) -> bool:
+    for e in vector:
+        if e != 0:
+            return False
+    else:
+        return True
+
+
+def data_wrapper(dataset, n_cluster: int) -> Tuple[List[DataPoint], List[GroundTruthCluster]]:
+    count = [0 for i in range(n_cluster)]
+    cc = [[] for i in range(n_cluster)]
+    data_stream = []
+    for index, cluster in enumerate(dataset[0]):
+        vector = dataset[1][index]
+        if is_zero_vector(vector):
+            continue
+        count[cluster] += 1
+        dp = BinaryVectorDataPoint(np.array(dataset[1][index]), str(cluster) + "-" + str(count[cluster]))
+        cc[cluster].append(dp)
+        data_stream.append(dp)
+    clusters = []
+    for c in cc:
+        clusters.append(GroundTruthCluster(c))
+    return data_stream, clusters
+
+
+if __name__ == "__main__":
+    # the setting of generating synthetic dataset
+    n_cluster = 4
+    n_point_each_cluster = 25
+    n_dim_datapoint = 2500
+    # generate synthetic dataset
+    gen = DataGeneration(shuffle=random_shuffle)  # other shuffle functions are provided in gendataset/shuffle.py
+    output = gen.gen_random_dataset(n_cluster=n_cluster, n_point_each_cluster=n_point_each_cluster,
+                                    n_dim_datapoint=n_dim_datapoint)
+    data_stream, ground_truth = data_wrapper(output, n_cluster)
+
+    # settings of Grinch algorithm
+    single_nn_search = False
+    k_nn = 25
+
+    single_elimination = False,
+
+    capping = False
+    capping_height = 100
+
+    navigable_small_world_graphs = False
+    k_nsw = 50
+    # grinch approach to cluster
+    # other linkage functions are provided in linkage/*.py
+    # customized linkage function is also allowed as long as it takes same inputs and return same outputs.
+    clustering = Grinch(cosine_similarity_for_binary_vector_data_point, debug=False,
+                        single_nn_search=single_nn_search, k_nn=k_nn,
+                        single_elimination=single_elimination,
+                        capping=capping, capping_height=capping_height,
+                        navigable_small_world_graphs=navigable_small_world_graphs, k_nsw=k_nsw)
+    # process data stream
+    for dp in data_stream:
+        clustering.insert(dp)
+    clustering.dendrogram.print()
+    print("dendrogram purity:", dendrogram_purity(ground_truth, clustering.dendrogram))
